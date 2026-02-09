59115a4dcf0bb3029460897973276c2692b865c0
a.riabinin <a.riabinin@notamedia.com.ru>
2026-02-09 13:20:59 +0300

    get url

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,67 +1,81 @@
 package main
 
-import(
-    "log/slog"
-    "os"
-    "url-shortener/internal/config"
-    "url-shortener/internal/lib/logger/sl"
-    "url-shortener/internal/storage/sqlite"
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
-const(
-    envLocal = "local"
-    envDev = "dev"
-    envProd = "prod"
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-    cfg := config.MustLoad()
+	cfg := config.MustLoad()
 
-    log := setupLogger(cfg.Env)
+	log := setupLogger(cfg.Env)
 
-//     log.With(slog.String("env", cfg.Env))
-    log.Info("starting url-shortener", slog.String("env", cfg.Env))
-    log.Debug("debug messages are enabled")
+	//     log.With(slog.String("env", cfg.Env))
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
-    storage, err := sqlite.New(cfg.StoragePath)
+	storage, err := sqlite.New(cfg.StoragePath)
 
-    if err != nil {
-        log.Error("failed to init storage", sl.Err(err))
-        os.Exit(1)
-    }
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
 
-    id, err := storage.SaveURL("https://google.com", "google")
-    if err != nil {
-        log.Error("failed to save url", sl.Err(err))
-        os.Exit(1)
-    }
+	url, err := storage.GetURL("google")
+	if err != nil {
+		log.Error("failed to get url", sl.Err(err))
+		os.Exit(1)
+	}
 
-    log.Info("saved url", slog.Int64("id", id))
+	log.Info("saved url is", slog.String("url", url))
 
-    id, err = storage.SaveURL("https://google.com", "google")
-    if err != nil {
-        log.Error("failed to save url", sl.Err(err))
-        os.Exit(1)
-    }
+	url, err = storage.GetURL("error")
+	if err != nil {
+		log.Error("failed to get url", sl.Err(err))
+		os.Exit(1)
+	}
 
-    _ = storage
+	// id, err := storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	//     log.Error("failed to save url", sl.Err(err))
+	//     os.Exit(1)
+	// }
+
+	// log.Info("saved url", slog.Int64("id", id))
+
+	// id, err = storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	//     log.Error("failed to save url", sl.Err(err))
+	//     os.Exit(1)
+	// }
+
+	_ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
-    var log * slog.Logger
-    switch env {
-        case envLocal:
-            log = slog.New(
-                slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-            )
-        case envDev:
-            log = slog.New(
-                slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-            )
-        case envProd :
-            log = slog.New(
-                slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-            )
-    }
-    return log
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,22 +1,24 @@
 package sqlite
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"errors"
+	"fmt"
+	"url-shortener/internal/storage"
+
 	"github.com/mattn/go-sqlite3"
-    "url-shortener/internal/storage"
 )
 
 type Storage struct {
-    db *sql.DB
+	db *sql.DB
 }
 
 func New(storagePath string) (*Storage, error) {
-    const op = "storage.sqlite.New"
+	const op = "storage.sqlite.New"
 
-    db, err := sql.Open("sqlite3", storagePath)
+	db, err := sql.Open("sqlite3", storagePath)
 
-    stmt, err := db.Prepare(`
+	stmt, err := db.Prepare(`
         CREATE TABLE IF NOT EXISTS url(
             id INTEGER PRIMARY KEY,
             alias TEXT NOT NULL UNIQUE,
@@ -24,41 +26,65 @@ func New(storagePath string) (*Storage, error) {
         CREATE INDEX IF NOT EXISTS idx_alias ON url(alias)
     `)
 
-    if err != nil {
-        return nil, fmt.Errorf("%s %w", op, err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", op, err)
+	}
 
-    _, err = stmt.Exec()
+	_, err = stmt.Exec()
 
-    if err != nil {
-        return nil, fmt.Errorf("%s %w", op, err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", op, err)
+	}
 
-    return &Storage{db: db}, nil
+	return &Storage{db: db}, nil
 }
 
-func (s *Storage) SaveURL (urlToSave string, alias string) (int64, error) {
-    const op = "storage.sqlite.SaveURL"
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
 
-    stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
-    if err != nil {
-        return 0, fmt.Errorf("%s: %w", op, err)
-    }
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
 
-    res, err := stmt.Exec(urlToSave, alias)
-    if err != nil {
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
 
-        if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
-            return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
-        }
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
 
-        return 0, fmt.Errorf("%s: %w", op, err)
-    }
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
 
-    id, err := res.LastInsertId()
-    if err != nil {
-        return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
-    }
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
 
-    return id, nil
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	var resURL string
+
+	err = stmt.QueryRow(alias).Scan(&resURL)
+
+	if err != nil {
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", storage.ErrURLNotFound
+		}
+
+		return "", fmt.Errorf("%s: execute statement: %w", op, err)
+	}
+
+	return resURL, nil
 }
