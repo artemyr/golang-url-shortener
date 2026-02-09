a887f907dcb5fd0f9707d2ac91b731ad5dfdc9d0
a.riabinin <a.riabinin@notamedia.com.ru>
2026-02-09 19:03:58 +0300

    +

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+в папке docker
+
+запустить контейнер
+
+`docker compose up go-app --build`
+
 установить какую-нибудь зависимость
+
 в корне проекта
-docker run --rm -v $(pwd):/app -w /app golang:1.25 go mod tidy
-docker run --rm -v $(pwd):/app -w /app golang:1.25 go get github.com/ilyakaznacheev/cleanenv
+
+`docker run --rm -v $(pwd):/app -w /app golang:1.25 go mod tidy`
+`docker run --rm -v $(pwd):/app -w /app golang:1.25 go get github.com/ilyakaznacheev/cleanenv`
 
 зайти посмотреть в контейнер
-docker compose run --rm go-app sh
+`docker compose run --rm go-app sh`
+
+dev режим
+`docker compose up go-dev --build`
+`docker compose exec go-dev sh`
--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,8 +4,13 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	mwLogger "url-shortener/internal/http-server/middleware/logger"
+	"url-shortener/internal/lib/logger/handlers/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -24,25 +29,31 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
-
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
 
-	url, err := storage.GetURL("google")
-	if err != nil {
-		log.Error("failed to get url", sl.Err(err))
-		os.Exit(1)
-	}
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
-	log.Info("saved url is", slog.String("url", url))
+	// url, err := storage.GetURL("google")
+	// if err != nil {
+	// 	log.Error("failed to get url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
 
-	url, err = storage.GetURL("error")
-	if err != nil {
-		log.Error("failed to get url", sl.Err(err))
-		os.Exit(1)
-	}
+	// log.Info("saved url is", slog.String("url", url))
+
+	// url, err = storage.GetURL("error")
+	// if err != nil {
+	// 	log.Error("failed to get url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
 
 	// id, err := storage.SaveURL("https://google.com", "google")
 	// if err != nil {
@@ -63,11 +74,10 @@ func main() {
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
+
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -76,6 +86,23 @@ func setupLogger(env string) *slog.Logger {
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
+
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
