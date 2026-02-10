aa18510e74d0da069264530f8b5c63444e94de2e
artemyr <artem.affetta@gmail.com>
2026-02-10 17:21:55 +0300

    redirect

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"url-shortener/internal/config"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/http-server/handlers/url/save"
+	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
@@ -43,6 +44,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
